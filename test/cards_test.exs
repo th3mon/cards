f88b21f9ba750262de740bb52364831233219feb
th3mon <p.szelenberger@gmail.com>
2023-09-10 21:34:45 +0200

    Create "create_deck makes a cards" test case

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,10 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes a cards" do
+    deck_length = length(Cards.create_deck())
+
+    assert deck_length == 52
+  end
 end
